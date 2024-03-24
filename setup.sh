f1da703d4e061b601961cef0e0398acee97d9e74
Zach Newton <znewton13@gmail.com>
2024-03-23 18:09:03 -0700

    setup sh fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 echo "Downloading fonts..."
 
+mkdir -p tmp
 (
-    mkdir tmp
     cd tmp
     curl "https://dl.dafont.com/dl/?f=punkboy" -L -o punkboy.zip
     unzip punkboy.zip
 )
+mkdir -p static
 cp tmp/punkboy_tbs.ttf static/punkboy.ttf
 
 rm -rf tmp
